146d8d8b663667d7b046b7617b54ce6b61dfec44
Richard Poole <richard.poole@digital.hmrc.gov.uk>
2018-03-28 16:43:44 +0100

    conditional expression added

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -4,7 +4,13 @@ import scala.io.StdIn
 
 object GreeterApplication extends App {
 
-  def greet(name: String) : Unit = println(s"Hello $name!")
+  def greet(name: String) : Unit = {
+    if (name == "Richard") {
+      println(s"Hello $name! you sexy son of a gun!!!!")
+    } else {
+      println(s"Hello $name")
+    }
+  }
   val name = StdIn.readLine("What is your name? ")
   greet(name)
 
