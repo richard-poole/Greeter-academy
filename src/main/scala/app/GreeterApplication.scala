b5e939af56d6efc4769cd4a7536692a671bc7d69
Richard Poole <richard.poole@digital.hmrc.gov.uk>
2018-03-29 13:28:10 +0100

    page 2 added

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -2,13 +2,23 @@ package app
 
 import scala.io.StdIn
 
+class Person (name: String) {
+  def speak():String = {
+    if (name == "Adam") {
+      s"You don't get a hello"
+    } else {
+      s"Hello $name"
+    }
+  }
+}
+
 object GreeterApplication extends App {
 
   def greet(name: String) : Unit = {
     if (name == "Richard") {
       println(s"Hello $name!!!!!")
     } else {
-      println(s"Hello $name")
+      println(s"Hello $name but you're not Richard!!!")
     }
   }
   val name = StdIn.readLine("What is your name? ")
