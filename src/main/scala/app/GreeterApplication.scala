d8025cf941433fcbd202727e2a6cc0f250618459
Richard Poole <richard.poole@digital.hmrc.gov.uk>
2018-04-30 10:57:22 +0100

    initial-setup part 2 pg8

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -6,19 +6,23 @@ object Prompt {
   def ask(message: String) = StdIn.readLine(message)
 }
 
-class Person(name: String) {
+
+class Person(name: String, age: Int) {
+  private val years: String = if (age > 1) "years" else "year"
+
   def speak(): String = {
     if (name == "Adam") {
       s"You don't get a hello"
     } else {
-      s"Hello $name"
+      s"Hello $name, you are $age $years old"
     }
   }
 }
 
 object GreeterApplication extends App {
   val name = Prompt.ask("What is your name? ")
-  val person = new Person(name)
+  val age = Prompt.ask("What is your age? ")
+  val person = new Person(name, age.toInt)
   println(person.speak())
 
 
