f6ddac17946a6b166bb824b0fdb0c1e557ab71a3
Richard Poole <richard.poole@digital.hmrc.gov.uk>
2018-05-31 12:22:04 +0100

    part 2 completed

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -1,5 +1,7 @@
 package app
 
+import java.time.temporal.TemporalAmount
+
 import scala.io.StdIn
 
 object Prompt {
@@ -7,14 +9,17 @@ object Prompt {
 }
 
 
-class Person(name: String, age: Int) {
+class Person(name: String, age: Int, private val bankAccount: BankAccount) {
+
+  def this (name: String, age: Int) = this(name, age, new SavingsAccount("12345", 0.00))
+
   private val years: String = if (age > 1) "years" else "year"
 
   def speak(): String = {
     if (name == "Adam") {
       s"You don't get a hello"
     } else {
-      s"Hello $name, you are $age $years old"
+      s"Hello $name, you are $age $years old. \n You have ${bankAccount.balance} in your account"
     }
   }
 }
@@ -22,8 +27,45 @@ class Person(name: String, age: Int) {
 object GreeterApplication extends App {
   val name = Prompt.ask("What is your name? ")
   val age = Prompt.ask("What is your age? ")
+
   val person = new Person(name, age.toInt)
   println(person.speak())
 
 
+}
+
+abstract class BankAccount(accountNumber: String, val balance: Double) {
+  def withdraw(amount: Double): BankAccount
+
+  def deposit(amount: Double): BankAccount
+}
+
+
+final class SavingsAccount(accountNumber: String, balance: Double) extends BankAccount(accountNumber, balance) {
+  override def withdraw(amount: Double): BankAccount = {
+    if ((balance - amount) < 0) {
+      println(s"You have insufficient funds, do one!!!!!")
+      this
+    } else {
+      val deducted = balance - amount
+      println(s"Balance after deductions: $deducted")
+      new SavingsAccount(accountNumber, deducted)
+    }
+  }
+
+  override def deposit(amount: Double): BankAccount = {
+    new SavingsAccount(accountNumber, balance + amount)
+  }
+}
+
+
+final class CashISAAccount(accountNumber: String, balance: Double) extends BankAccount(accountNumber, balance) {
+  override def withdraw(amount: Double): BankAccount = {
+    println(s"You cannot withdraw from this ISA, do one!!!!!")
+    this
+  }
+
+override def deposit (amount: Double): BankAccount = {
+  new CashISAAccount (accountNumber, balance + amount)
+}
 }
