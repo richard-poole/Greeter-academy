4df1d683d2801d2cd102001c3107e6fffb76efb9
Richard Poole <richard.poole@digital.hmrc.gov.uk>
2018-06-13 16:16:35 +0100

    part 6 up to page 10

--- src/main/scala/app/models/Pet.scala
+++ src/main/scala/app/models/Pet.scala
@@ -2,12 +2,34 @@ package app.models
 
 sealed trait Pet {
   val name: String
-  def speak(): String
+
+  def speak(): String = "hello"
 }
 
-final class Cat(override val name: String) extends Pet {
-  override def speak() : String = name + "says meow"
+final case class Cat(override val name: String) extends Pet {
+ override def speak(): String = name + "says meow"
 }
-final class Dog (override val name: String) extends Pet {
-  override def speak(): String = name + "says Woof"
+
+final case class Dog(override val name: String) extends Pet {
+ // override def speak(): String = name + "says Woof"
+}
+
+object Stuff {
+
+  def whoDis(pet: Pet) = {
+    pet match {
+      case Dog(name) => s"This is a dog and their name is $name"
+      case Cat(name) => s"This is a cat and their name is $name"
+    }
+
+  }
+
+  def main(args: Array[String]) : Unit = {
+
+    val d = Dog("Geoff")
+    println(whoDis(d))
+
+  }
+
+
 }
--- src/main/scala/app/repositories/PetRepository.scala
+++ src/main/scala/app/repositories/PetRepository.scala
@@ -1,10 +1,46 @@
 package app.repositories
 
-import app.models.Pet
-
 import scala.collection.mutable.ArrayBuffer
 
+sealed trait Pet {
+  val name: String
+
+  def speak(): String = "hello"
+}
+
 object PetRepository {
+
+  val c1 = Cat("Tom")
+  val c2 = Dog("Spot")
+  val c3 = Cat("Felix")
+
+  c1 == c3
+
+  println(c1.name)
+
+  case class Cat (name: String) extends Pet {
+    override def speak(): String = name + "says Meow!!"
+  }
+//
+//  case class Dog (name: String) extends Pet {
+//    override def speak(): String = name + "says Woof!!"
+//  }
+
+  class Dog (name: String, private val personYears: Int) {
+    import Dog._
+    def age = calculateAgeInDogYears(personYears)
+
+  }
+  object Dog {
+    def withName(name:String) = new Dog(name, 2)
+    def apply (name: String, age: Int =1) = new Dog(name, age)
+    private def calculateAgeInDogYears(age: Int) = age * 7
+  }
+
+  val d = Dog("Bruce", 3)
+
+
+
 
   private	val	_pets	:	ArrayBuffer[Pet]	=	new	ArrayBuffer[Pet]()
 
@@ -25,3 +61,4 @@ object PetRepository {
   def update(pet : Pet) : List[Pet] = _pets.toList
 
 }
+
